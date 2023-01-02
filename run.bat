0f0471a0afd157531e2a3329d386c4375d9c7edd
optimisme <optimisme@gmail.com>
2023-01-02 19:49:12 +0100

    Fixed for windows 2

--- run.bat
+++ run.bat
@@ -7,5 +7,5 @@ rm -r -force .\lib\javafx-windows\lib\.*
 mkdir bin
 xcopy .\assets .\bin\assets /E /I /Y
 
-javac -cp ".:.\\bin:.\\lib\\password4j-1.6.2.jar:.\\lib\\slf4j-simple-1.6.1.jar:.\\lib\\slf4j.api-1.6.1.jar" -d .\bin\ .\src\*.java
-java -cp ".:.\\bin:.\\lib\\password4j-1.6.2.jar:.\\lib\\slf4j-simple-1.6.1.jar:.\\lib\\slf4j.api-1.6.1.jar" Main
+javac -cp ".;.\\bin;.\\lib\\password4j-1.6.2.jar;.\\lib\\slf4j-simple-1.6.1.jar;.\\lib\\slf4j.api-1.6.1.jar" -d .\bin\ .\src\*.java
+java -cp ".;.\\bin;.\\lib\\password4j-1.6.2.jar;.\\lib\\slf4j-simple-1.6.1.jar;.\\lib\\slf4j.api-1.6.1.jar" Main
